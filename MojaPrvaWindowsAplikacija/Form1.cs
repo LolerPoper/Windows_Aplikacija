88c44119f96bf715a04d9c9aee13393ea169c2d1
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2022-09-21 09:32:12 +0200

    Add files via upload

---
+++ MojaPrvaWindowsAplikacija/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MojaPrvaWindowsAplikacija
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIzracun_Click(object sender, EventArgs e)
+        {
+            int brojA, brojB, zbroj;
+
+            brojA = Convert.ToInt32(txtUnosBrojaA.Text);
+            brojB = Convert.ToInt32(txtUnosBrojaB.Text);
+            zbroj = brojA + brojB;
+
+            txtRezultat.Text = Convert.ToString(zbroj);
+        }
+
+        private void txtRezultat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
